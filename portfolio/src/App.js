1e9428bdb01d081deab361774fc1baaa4512796c
Antquanique Dancy <antdancy95@gmail.com>
2020-02-08 22:15:23 -0500

    added Route for the landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Landing from './Components/Landing'
 import NavBar from './Components/NavBar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Landing/>
+    <Router>
+      <div>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+        </Switch>
+        
+      </div>
+    </Router>
+     
     </div>
   );
 }
