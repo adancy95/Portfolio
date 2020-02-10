ff265ebd946c95e62fc7adf69e2cc741736eb060
Antquanique Dancy <antdancy95@gmail.com>
2020-02-09 21:20:01 -0500

    added styling to the navbar

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import {BrowserRouter as Router,NavLink} from "react-router-dom";
-  
+import '../App.css'  
 
 const NavBar = () => {
     return(
         <Router>
-            <div>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About Me</NavLink>
-                    </li>
-                    <li> 
-                        <NavLink to="/mywork">My Work</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/resume">Resume</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contactme">Contact Me</NavLink>
-                    </li>
-                </ul>
-            </div>
+            <header>
+                <nav className="nav-bar">
+                 <NavLink className="nav-links nav-logo" to="/">Ant-quanique Dancy</NavLink>
+                    <ul className="nav-ul" >
+                        <li>
+                            <NavLink className="nav-links" to="/about">About Me</NavLink>
+                        </li>
+                        <li> 
+                            <NavLink className="nav-links" to="/mywork">My Work</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="nav-links" to="/resume">Resume</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="nav-links nav-btn" to="/contactme">Contact Me</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </Router>
         
     )
