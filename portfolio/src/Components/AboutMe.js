d3c99f41b7f7c37060cfeb6b1ac2c85784ae91cc
Antquanique Dancy <antdancy95@gmail.com>
2020-02-10 19:09:58 -0500

    added About me component

---
+++ portfolio/src/Components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return(
+        <div>
+            <h1 className="aboutme-header">About Me</h1>
+        </div>
+    )
+}
+
+export default AboutMe
