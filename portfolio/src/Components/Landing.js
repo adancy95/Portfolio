a308069025aceb4e841c8ac9c445cf81ada18145
Antquanique Dancy <antdancy95@gmail.com>
2020-02-10 19:09:22 -0500

    added image and restructured content

--- portfolio/src/Components/Landing.js
+++ portfolio/src/Components/Landing.js
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 
 const Landing = () => {
     return(
-        <div>
-            <h1>Hello Everybody.</h1>
-            <h1>First name Antquanique</h1>
-            <h1>Last name Dancy </h1>
-            <h1> Also known as Ant, the</h1>
-            <h1> Ex QA Engineer turned developer</h1>
+        <div className="jumbotron">
+            <div>
+                <img className="jumbotron-img" src="/antheadshot.svg" alt="Antquanique's headshot"/>
+            </div>
+            <div className="jumbotron-text jumbotron-text-margin">
+                <h1 className="jumbotron-text-margin">Ant-quanique Dancy</h1>
+                <h4 className="jumbotron-text-margin">[Ant-qua-nique Dan-cy] (noun)</h4>
+                <hr className="jumbotron-text-margin"/>
+                <ol>
+                    <li className="jumbotron-text-margin">AKA Ant (if you can't pronounce my first name)</li>
+                    <li className="jumbotron-text-margin">The Ex QA Engineer turned developer who is passonate about breaking down complex problems, and solving those problems with code.</li>
+                </ol>
+                <Link className="jumbotron-btn" to="/aboutme">About Me</Link>
+            </div>
+           
         </div>
     )
 }
