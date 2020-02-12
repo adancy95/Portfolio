dfdc724788aa7db65459a70e96e49c87fad0f033
Antquanique Dancy <antdancy95@gmail.com>
2020-02-11 19:15:47 -0500

    added section header

---
+++ portfolio/src/Components/MyWork.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MyWork = () => {
+    return(
+        <section>
+            <h1 className="section-header">My Work</h1>
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default MyWork
