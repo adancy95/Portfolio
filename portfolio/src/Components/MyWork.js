bd8215c26191bcc3977e92318b5d55f3ac1c6f26
Antquanique Dancy <antdancy95@gmail.com>
2020-02-12 17:43:14 -0500

    Added one project component to the mywork section

--- portfolio/src/Components/MyWork.js
+++ portfolio/src/Components/MyWork.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const MyWork = () => {
     return(
         <section>
             <h1 className="section-header">My Work</h1>
             <div>
-
-            </div>
+                <ProjectCard title="Connect4" technology="JavaScript, jQuery, SCSS, Git, Bootstrap, HTML, CSS" projectImage="https://i.pinimg.com/736x/e0/26/e3/e026e372c1c67454cde3e2e00c2c19fb.jpg"
+/>            </div>
         </section>
     )
 }
