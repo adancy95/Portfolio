be0075de4a16f06f1392bb4a84e1e884b27f89d5
Antquanique Dancy <antdancy95@gmail.com>
2020-02-11 19:14:34 -0500

    added content to the about me section

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {FaRegFileCode} from "react-icons/fa";
+import { GiStoneCrafting } from "react-icons/gi";
+import { GiBrain } from "react-icons/gi";
 
 const AboutMe = () => {
     return(
-        <div>
-            <h1 className="aboutme-header">About Me</h1>
-        </div>
+        <section>
+            <h1 className="section-header">About Me</h1>
+            <div className="aboutme">
+                <div className="aboutme-section">
+                    <FaRegFileCode className="aboutme-icon"/>
+                    <h3>Developer</h3>
+                    <p>I enjoy creating software solutions that people the pain points of business</p>
+                </div>
+                <div className="aboutme-section">
+                    <GiBrain className="aboutme-icon"/>
+                    <h3>Problem Solver</h3>
+                    <p>I’m a developer who likes to experiment through trial and error</p>
+                </div>
+                <div className="aboutme-section">
+                    <GiStoneCrafting className="aboutme-icon"/>
+                    <h3>Diyer</h3>
+                    <p>I’m a die hard diyer, and a pinterest fanatic who has a knack for making crafts.</p>
+                </div>
+            </div>
+           
+
+        </section>
     )
 }
 
