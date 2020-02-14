4572d49daab58d8941edb5d1322ccd5528dcc2f6
Antquanique Dancy <antdancy95@gmail.com>
2020-02-13 21:50:29 -0500

    added a contact component

---
+++ portfolio/src/Components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+       <section>
+           <h1 className="section-header">Contact Me</h1>
+           <ul>
+               <li>Linkedin</li>
+               <li>Github</li>
+           </ul>
+       </section>
+    )
+}
+
+export default Contact
