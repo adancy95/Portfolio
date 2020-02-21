e934777a6b0464473fb0617b67f8cdf5e6de4d88
Antquanique Dancy <antdancy95@gmail.com>
2020-02-20 21:02:02 -0500

    Fixed Typo in the developer column

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -5,21 +5,21 @@ import { GiBrain } from "react-icons/gi";
 
 const AboutMe = () => {
     return(
-        <section>
+        <section id="aboutMe">
             <h1 className="section-header">About Me</h1>
             <div className="aboutme">
                 <div className="aboutme-section">
-                    <FaRegFileCode className="aboutme-icon"/>
+                    <FaRegFileCode className="icon"/>
                     <h3>Developer</h3>
-                    <p>I enjoy creating software solutions that people the pain points of business</p>
+                    <p>I enjoy creating software solutions that solve people's pain points</p>
                 </div>
                 <div className="aboutme-section">
-                    <GiBrain className="aboutme-icon"/>
+                    <GiBrain className="icon"/>
                     <h3>Problem Solver</h3>
                     <p>I’m a developer who likes to experiment through trial and error</p>
                 </div>
                 <div className="aboutme-section">
-                    <GiStoneCrafting className="aboutme-icon"/>
+                    <GiStoneCrafting className="icon"/>
                     <h3>Diyer</h3>
                     <p>I’m a die hard diyer, and a pinterest fanatic who has a knack for making crafts.</p>
                 </div>
