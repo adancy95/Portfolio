895e89a900e025f08df3be58c82adeefbda18a22
Antquanique Dancy <antdancy95@gmail.com>
2020-02-24 21:34:49 -0500

    Updates

---
+++ precache-manifest.ee5f1f98bd3e815a3fe94950dc53c015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00da271120b53ec98a70396cb803528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54f1ded66466cd984e3",
+    "url": "/static/css/main.456dc8ac.chunk.css"
+  },
+  {
+    "revision": "adc0ce1058e58b84d6d0",
+    "url": "/static/js/2.811c6d50.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.811c6d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54f1ded66466cd984e3",
+    "url": "/static/js/main.bb9ecac3.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
