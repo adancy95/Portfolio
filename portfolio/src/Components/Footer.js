04eeeaa1242a3bbf131ada333f71c52d1108f5aa
Antquanique Dancy <antdancy95@gmail.com>
2020-02-24 21:23:38 -0500

    added a footer section

---
+++ portfolio/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <p>Ant-quanique Dancy</p>
+        </footer>
+    )
+}
+
+export default Footer
