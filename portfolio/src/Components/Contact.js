155c60a2cdba47a94119ee6587f1c16fc7375e6c
Antquanique Dancy <antdancy95@gmail.com>
2020-02-24 21:21:07 -0500

    added social links to contact section

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import { FaGithub, FaLinkedinIn} from "react-icons/fa"
+import { MdEmail } from "react-icons/md";
 const Contact = () => {
     return(
-       <section>
+       <section id="contact">
            <h1 className="section-header">Contact Me</h1>
-           <ul>
-               <li>Linkedin</li>
-               <li>Github</li>
+           <ul className="contact">
+               <li><a rel="noopener noreferrer" href="https://www.linkedin.com/in/adancy95/" target="_blank"><FaLinkedinIn className="icon"/></a></li>
+               <li><a rel="noopener noreferrer" href="https://github.com/adancy95" target="_blank"><FaGithub className="icon"/></a></li>
+               <li><a rel="noopener noreferrer" href="mailto:antdancy95@gmail.com" target="_blank"><MdEmail className="icon"/></a></li>
            </ul>
        </section>
     )
