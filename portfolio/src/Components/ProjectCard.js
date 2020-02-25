51bcbe1a10191baad325041943eb2b1157b55a58
Antquanique Dancy <antdancy95@gmail.com>
2020-02-24 21:23:23 -0500

    fixed external link bug

--- portfolio/src/Components/ProjectCard.js
+++ portfolio/src/Components/ProjectCard.js
@@ -6,7 +6,7 @@ const ProjectCard = (props) => {
             <img src={props.projectImage} alt={props.title} className="card-image" />
             <div className="card-overlay">
                 <div className="card-text">
-                    <h2 className="project-title"><a href="{props.url}">{props.title}</a></h2>
+                    <h2 className="project-title"><a rel="noopener noreferrer" href={props.url} target="_blank">{props.title}</a></h2>
                 </div>
             </div>
             
