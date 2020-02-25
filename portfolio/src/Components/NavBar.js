660af6f4102001a662c35cdfe3e49c05422b68c7
Antquanique Dancy <antdancy95@gmail.com>
2020-02-24 21:19:06 -0500

    modified nav links to use hash links

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router,NavLink} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 import '../App.css'  
 
 const NavBar = () => {
@@ -10,13 +11,13 @@ const NavBar = () => {
                  <NavLink className="nav-links nav-logo" to="/">Ant-quanique Dancy</NavLink>
                     <ul className="nav-ul" >
                         <li>
-                            <NavLink className="nav-links" to="/about">About Me</NavLink>
+                            <NavLink className="nav-links" to="/#aboutMe">About Me</NavLink>
                         </li>
                         <li> 
-                            <NavLink className="nav-links" to="/mywork">My Work</NavLink>
+                            <NavLink className="nav-links" to="/#myWork">My Work</NavLink>
                         </li>
                         <li>
-                            <NavLink className="nav-links nav-btn" to="/contactme">Contact Me</NavLink>
+                            <NavLink className="nav-links nav-btn" to="/#contact">Contact Me</NavLink>
                         </li>
                     </ul>
                 </nav>
